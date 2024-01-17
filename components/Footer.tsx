9cc4fc871369ef1b9db70e28c7815e099bc0a799
iiskakov <tradeshells@gmail.com>
2024-01-17 13:43:59 +0000

    Modify Footer in components/Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,14 +3,121 @@ import siteMetadata from '@/data/siteMetadata'
 import SocialIcon from '@/components/social-icons'
 
 export default function Footer() {
-  return (
+  
+ const getCookie = name => {
+        const cookies = document.cookie.split('; ').reduce((acc, cookie) => {
+            const [key, value] = cookie.split('=');
+            acc[key] = value;
+            return acc;
+        }, {});
+        return cookies[name] || '';
+    };
+
+    const cookieValue = getCookie('USER_EXPERIMENT');
+    const bucket = cookieValue ? cookieValue.split('.')[0] : '';
+
+    const A = (
+            
     <footer>
       <div className="mt-16 flex flex-col items-center">
         <div className="mb-3 flex space-x-4">
           <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
           <SocialIcon kind="github" href={siteMetadata.github} size={6} />
           {/* <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} />
-          <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} /> */}
+           <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} /> */}
+          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
+          <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
+        </div>
+        <div className="mb-2 flex space-x-2 text-sm text-gray-600 dark:text-gray-300">
+          <div>{siteMetadata.author}</div>
+          <div>{` • `}</div>
+          <div>{`© ${new Date().getFullYear()}`}</div>
+          <div>{` • `}</div>
+          <Link href="/">{siteMetadata.title}</Link>
+        </div>
+        {/* <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
+          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
+            Tailwind Nextjs Theme
+          </Link>
+         </div> */}
+      </div>
+    </footer>
+
+
+    );
+
+    const B = (
+            <footer>
+  <div className="mt-16 flex flex-col items-center">
+    <div className="mb-3 flex space-x-4">
+      <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
+      <SocialIcon kind="github" href={siteMetadata.github} size={6} />
+      {/* <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} />
+       <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} /> */}
+      <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
+      <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
+    </div>
+    <div className="mb-2 flex space-x-2 text-sm text-gray-600 dark:text-gray-300">
+      <div>{siteMetadata.author}</div>
+      <div>{` • `}</div>
+      <div>{`© ${new Date().getFullYear()}`}</div>
+      <div>{` • `}</div>
+      <Link href="/">{siteMetadata.title}</Link>
+    </div>
+    {/* <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
+      <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
+        Tailwind Nextjs Theme
+      </Link>
+     </div> */}
+  </div>
+</footer>
+
+    );
+
+    const C = (
+            <footer>
+  <div className="mt-16 flex flex-col items-center">
+    <div className="mb-3 flex space-x-4">
+      <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
+      <SocialIcon kind="github" href={siteMetadata.github} size={6} />
+      {/* <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} />*/}
+      {/* <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} />*/}
+      <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
+      <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
+    </div>
+    <div className="mb-2 flex space-x-2 text-sm text-gray-600 dark:text-gray-300">
+      <div className="font-semibold">{siteMetadata.author}</div>
+      <div>{` • `}</div>
+      <div className="font-semibold">{`© ${new Date().getFullYear()}`}</div>
+      <div>{` • `}</div>
+      <Link href="/" className="text-blue-600 hover:text-blue-800 dark:hover:text-blue-400">{siteMetadata.title}</Link>
+    </div>
+    {/* <div className="mb-8 text-sm text-gray-600 dark:text-gray-300">
+      <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog" className="hover:underline">
+        Tailwind Nextjs Theme
+      </Link>
+    </div>*/}
+  </div>
+</footer>
+
+    );
+
+    switch (bucket) {
+        case 'A':
+            return A;
+        case 'B':
+            return B;
+        case 'C':
+            return C;
+        default:
+            return (
+                <footer>
+      <div className="mt-16 flex flex-col items-center">
+        <div className="mb-3 flex space-x-4">
+          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
+          <SocialIcon kind="github" href={siteMetadata.github} size={6} />
+          {/* <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} />
+           <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} /> */}
           <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
           <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
         </div>
@@ -25,8 +132,10 @@ export default function Footer() {
           <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
             Tailwind Nextjs Theme
           </Link>
-        </div> */}
+         </div> */}
       </div>
     </footer>
-  )
+            );
+    }
+    
 }

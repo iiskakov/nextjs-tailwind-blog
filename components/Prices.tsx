ef87db0dc23844e2f062987987673a4db4e74223
Iskander <tradeshells@gmail.com>
2024-01-20 19:48:30 +0600

    smaller prices

--- components/Prices.tsx
+++ components/Prices.tsx
@@ -20,13 +20,16 @@ export default function Prices() {
 
     return (
         <div>
-            <div className="container ">
-                <div className="grid text-black grid-cols-1 gap-6 mt-2 md:grid-cols-3 md:gap-8">
-                    <div className="flex flex-col p-6 bg-white shadow-lg rounded-lg dark:bg-zinc-850 justify-between border border-gray-300">
+            <div className="container">
+                <div className="grid text-black grid-cols-1 gap-6 mt-2 md:grid-cols-2 md:gap-8">
+                    <div className="relative flex flex-col p-6 bg-white shadow-lg rounded-lg dark:bg-zinc-850 justify-between border border-gray-300">
+                        <div className="px-3 py-1 text-sm text-white bg-gradient-to-r from-pink-500 to-purple-500 rounded-full inline-block absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                            Popular
+                        </div>
                         <div>
-                            <h3 className="text-2xl font-bold text-center">Substack</h3>
+                            <h3 className="text-2xl font-bold text-center">Premium</h3>
                             <div className="mt-4 text-center text-zinc-600 dark:text-zinc-400">
-                                <span className="text-4xl font-bold">$9</span>/ month
+                                <span className="text-4xl font-bold">$19</span>/ month
                             </div>
                             <ul className="mt-4 space-y-2">
                                 <li className="flex items-center">
@@ -41,26 +44,6 @@ export default function Prices() {
                                     <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
                                     Community Forum Access
                                 </li>
-                            </ul>
-                        </div>
-                        <div className="mt-6">
-                            <button className="w-full">Subscribe</button>
-                        </div>
-                    </div>
-                    <div className="relative flex flex-col p-6 bg-white shadow-lg rounded-lg dark:bg-zinc-850 justify-between border border-purple-500">
-                        <div className="px-3 py-1 text-sm text-white bg-gradient-to-r from-pink-500 to-purple-500 rounded-full inline-block absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                            Popular
-                        </div>
-                        <div>
-                            <h3 className="text-2xl font-bold text-center">Pro</h3>
-                            <div className="mt-4 text-center text-zinc-600 dark:text-zinc-400">
-                                <span className="text-4xl font-bold">$19</span>/ month
-                            </div>
-                            <ul className="mt-4 space-y-2">
-                                <li className="flex items-center">
-                                    <CheckIcon className="text-white text-2xs bg-green-500 rounded-full mr-2 p-1" />
-                                    All Basic Features Plus
-                                </li>
                                 <li className="flex items-center">
                                     <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
                                     Exclusive In-Depth Tutorials
@@ -72,19 +55,19 @@ export default function Prices() {
                             </ul>
                         </div>
                         <div className="mt-6">
-                            <button className="w-full text-white bg-gradient-to-r from-pink-500 to-purple-500">Subscribe</button>
+                            <button className="w-full">Subscribe</button>
                         </div>
                     </div>
-                    <div className="flex flex-col p-6 bg-white shadow-lg rounded-lg dark:bg-zinc-850 justify-between border border-gray-300">
+                    <div className="flex flex-col p-6 bg-white shadow-lg rounded-lg dark:bg-zinc-850 justify-between border border-purple-500">
                         <div>
-                            <h3 className="text-2xl font-bold text-center">Enterprise</h3>
+                            <h3 className="text-2xl font-bold text-center">Professional</h3>
                             <div className="mt-4 text-center text-zinc-600 dark:text-zinc-400">
                                 <span className="text-4xl font-bold">$49</span>/ month
                             </div>
                             <ul className="mt-4 space-y-2">
                                 <li className="flex items-center">
                                     <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
-                                    All Pro Features Plus
+                                    All Premium Features Plus
                                 </li>
                                 <li className="flex items-center">
                                     <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
@@ -93,11 +76,12 @@ export default function Prices() {
                             </ul>
                         </div>
                         <div className="mt-6">
-                            <button className="w-full">Subscribe</button>
+                            <button className="w-full text-white bg-gradient-to-r from-pink-500 to-purple-500">Subscribe</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
     )
 }

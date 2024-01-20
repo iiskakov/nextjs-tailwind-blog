1d6cf401f4d95b7a47a01ddf7d47b1858a40275a
Iskander <tradeshells@gmail.com>
2024-01-20 18:30:07 +0600

    Subscription page added

---
+++ components/Prices.tsx
@@ -0,0 +1,124 @@
+export default function Prices() {
+    return (
+        <>
+            <section className="min-h-screen w-full py-12 from-gray-50 to-gray-100 dark:from-zinc-900 dark:to-zinc-800 flex items-center justify-center">
+                <div className="container px-4 md:px-6">
+                    <div className="grid text-black grid-cols-1 gap-6 mt-8 md:grid-cols-3 md:gap-8">
+
+                        <div className="flex flex-col p-6 bg-white shadow-lg rounded-lg dark:bg-zinc-850 justify-between border border-gray-300">
+                            <div>
+                                <h3 className="text-2xl font-bold text-center">Basic</h3>
+                                <div className="mt-4 text-center text-zinc-600 dark:text-zinc-400">
+                                    <span className="text-4xl font-bold">$9</span>/ month
+                                </div>
+                                <ul className="mt-4 space-y-2">
+                                    <li className="flex items-center">
+                                        <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                                        Access to Standard Articles
+                                    </li>
+                                    <li className="flex items-center">
+                                        <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                                        Monthly Newsletter
+                                    </li>
+                                    <li className="flex items-center">
+                                        <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                                        Community Forum Access
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="mt-6">
+                                <button className="w-full">Subscribe</button>
+                            </div>
+                        </div>
+
+                        <div className="relative flex flex-col p-6 bg-white shadow-lg rounded-lg dark:bg-zinc-850 justify-between border border-purple-500">
+                            <div className="px-3 py-1 text-sm text-white bg-gradient-to-r from-pink-500 to-purple-500 rounded-full inline-block absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                                Popular
+                            </div>
+                            <div>
+                                <h3 className="text-2xl font-bold text-center">Pro</h3>
+                                <div className="mt-4 text-center text-zinc-600 dark:text-zinc-400">
+                                    <span className="text-4xl font-bold">$19</span>/ month
+                                </div>
+                                <ul className="mt-4 space-y-2">
+                                    <li className="flex items-center">
+                                        <CheckIcon className="text-white text-2xs bg-green-500 rounded-full mr-2 p-1" />
+                                        All Basic Features Plus
+                                    </li>
+                                    <li className="flex items-center">
+                                        <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                                        Exclusive In-Depth Tutorials
+                                    </li>
+                                    <li className="flex items-center">
+                                        <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                                        Early Access to New Content
+                                    </li>
+                                    <li className="flex items-center">
+                                        <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                                        Ad-Free Experience
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="mt-6">
+                                <button className="w-full bg-gradient-to-r from-pink-500 to-purple-500">Subscribe</button>
+                            </div>
+                        </div>
+                        <div className="flex flex-col p-6 bg-white shadow-lg rounded-lg dark:bg-zinc-850 justify-between border border-gray-300">
+                            <div>
+                                <h3 className="text-2xl font-bold text-center">Enterprise</h3>
+                                <div className="mt-4 text-center text-zinc-600 dark:text-zinc-400">
+                                    <span className="text-4xl font-bold">$49</span>/ month
+                                </div>
+                                <ul className="mt-4 space-y-2">
+                                    <li className="flex items-center">
+                                        <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                                        All Pro Features Plus
+                                    </li>
+                                    <li className="flex items-center">
+                                        <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                                        One-on-One Mentorship Sessions
+                                    </li>
+                                    <li className="flex items-center">
+                                        <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                                        Access to Exclusive Webinars
+                                    </li>
+                                    <li className="flex items-center">
+                                        <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                                        Customizable Learning Paths
+                                    </li>
+                                    <li className="flex items-center">
+                                        <CheckIcon className="text-white text-xs bg-green-500 rounded-full mr-2 p-1" />
+                                        Corporate License for Teams
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="mt-6">
+                                <button className="w-full">Subscribe</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+
+    )
+}
+
+function CheckIcon(props) {
+    return (
+        <svg
+            {...props}
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        >
+            <polyline points="20 6 9 17 4 12" />
+        </svg>
+    )
+}
--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,10 +1,10 @@
 const headerNavLinks = [
-  { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  // { href: '/projects', title: 'Projects' },
-  // { href: '/about', title: 'About' },
-  { href: 'https://resume.min71.dev', title: 'Resume' },
+    { href: '/', title: 'Home' },
+    { href: '/blog', title: 'Blog' },
+    { href: '/tags', title: 'Subscription' },
+    // { href: '/projects', title: 'Projects' },
+    // { href: '/about', title: 'About' },
+    { href: 'https://resume.min71.dev', title: 'Resume' },
 ]
 
 export default headerNavLinks
